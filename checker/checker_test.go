16de9926943e9cbeb40f940732269b08529b3e32
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-05-29 18:42:48 +0900

    add error tests for checker

--- checker/checker_test.go
+++ checker/checker_test.go
@@ -8,6 +8,7 @@ package checker
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
@@ -97,5 +98,122 @@ func TestNotDuplicate(t *testing.T) {
 	)
 	result, err := checker.IsDuplicate("test")
 	assert.False(result)
+	assert.NoError(err)
+}
+
+func TestRaiseError1(t *testing.T) {
+	assert := assert.New(t)
+	kapi, tearDown := setUpChecker(t)
+	defer tearDown()
+
+	config := conf.NewConfig()
+	checker, err := NewChecker(config)
+
+	assert.NotNil(checker)
+	assert.NoError(err)
+
+	lockOptions := &client.SetOptions{
+		PrevExist: client.PrevNoExist,
+		TTL:       time.Second * time.Duration(config.LockTTL),
+	}
+	raisedError := errors.New("error")
+
+	gomock.InOrder(
+		kapi.EXPECT().Set(context.TODO(), "/lock/test", "mutexID", lockOptions).Return(nil, raisedError).AnyTimes(),
+	)
+	result, err := checker.IsDuplicate("test")
+	assert.True(result)
+	assert.Equal(raisedError, err)
+}
+
+func TestRaiseError2(t *testing.T) {
+	assert := assert.New(t)
+	kapi, tearDown := setUpChecker(t)
+	defer tearDown()
+
+	config := conf.NewConfig()
+	checker, err := NewChecker(config)
+
+	assert.NotNil(checker)
+	assert.NoError(err)
+
+	lockOptions := &client.SetOptions{
+		PrevExist: client.PrevNoExist,
+		TTL:       time.Second * time.Duration(config.LockTTL),
+	}
+	raisedError := errors.New("error")
+
+	gomock.InOrder(
+		kapi.EXPECT().Set(context.TODO(), "/lock/test", "mutexID", lockOptions).Return(nil, nil),
+		kapi.EXPECT().Get(context.Background(), "/data/test", nil).Return(nil, raisedError),
+		kapi.EXPECT().Delete(context.TODO(), "/lock/test", nil).Return(nil, nil),
+	)
+	result, err := checker.IsDuplicate("test")
+	assert.True(result)
+	assert.Equal(raisedError, err)
+}
+
+func TestRaiseError3(t *testing.T) {
+	assert := assert.New(t)
+	kapi, tearDown := setUpChecker(t)
+	defer tearDown()
+
+	config := conf.NewConfig()
+	checker, err := NewChecker(config)
+
+	assert.NotNil(checker)
+	assert.NoError(err)
+
+	lockOptions := &client.SetOptions{
+		PrevExist: client.PrevNoExist,
+		TTL:       time.Second * time.Duration(config.LockTTL),
+	}
+	dataOptions := &client.SetOptions{
+		PrevExist: client.PrevNoExist,
+		TTL:       time.Second * time.Duration(config.DataTTL),
+	}
+	keyNotFound := client.Error{
+		Code:    client.ErrorCodeKeyNotFound,
+		Message: "test message",
+		Cause:   "test cause",
+		Index:   0,
+	}
+	raisedError := errors.New("error")
+
+	gomock.InOrder(
+		kapi.EXPECT().Set(context.TODO(), "/lock/test", "mutexID", lockOptions).Return(nil, nil),
+		kapi.EXPECT().Get(context.Background(), "/data/test", nil).Return(nil, keyNotFound),
+		kapi.EXPECT().Set(context.Background(), "/data/test", "duplicate", dataOptions).Return(nil, raisedError),
+		kapi.EXPECT().Delete(context.TODO(), "/lock/test", nil).Return(nil, nil),
+	)
+	result, err := checker.IsDuplicate("test")
+	assert.True(result)
+	assert.Equal(raisedError, err)
+}
+
+func TestRaiseError4(t *testing.T) {
+	assert := assert.New(t)
+	kapi, tearDown := setUpChecker(t)
+	defer tearDown()
+
+	config := conf.NewConfig()
+	checker, err := NewChecker(config)
+
+	assert.NotNil(checker)
+	assert.NoError(err)
+
+	lockOptions := &client.SetOptions{
+		PrevExist: client.PrevNoExist,
+		TTL:       time.Second * time.Duration(config.LockTTL),
+	}
+	raisedError := errors.New("error")
+
+	gomock.InOrder(
+		kapi.EXPECT().Set(context.TODO(), "/lock/test", "mutexID", lockOptions).Return(nil, nil),
+		kapi.EXPECT().Get(context.Background(), "/data/test", nil).Return(nil, nil),
+		kapi.EXPECT().Delete(context.TODO(), "/lock/test", nil).Return(nil, raisedError).AnyTimes(),
+	)
+	result, err := checker.IsDuplicate("test")
+	assert.True(result)
 	assert.NoError(err)
 }
